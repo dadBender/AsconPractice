e7bf03174cf4c921bf4a5011f1860381773bc071
Anton Efimov <ntnefimov@gmail.com>
2025-07-25 23:10:16 +0800

    v0.3

--- src/entities/user/model/userSlice.ts
+++ src/entities/user/model/userSlice.ts
@@ -12,7 +12,7 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser(state, action: PayloadAction<User>) {
+    setUser(state, action: PayloadAction) {
       return action.payload;
     },
     clearUser() {
